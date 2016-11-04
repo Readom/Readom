a44c7229a255cf3c253f63417ce55a9911a8692d
Liu Lantao <liulantao@gmail.com>
2016-11-04 12:25:23 +0800

    use Readom-API-Server

--- app/readom.rb
+++ app/readom.rb
@@ -1,28 +1,43 @@
 class Readom
-  def self.fetch_item_sample(list=:newstories, &block)
-    listEntry = 'https://Readom.github.io/HackerNewsJSON/%s.json' % list
+  def self.fetch_item(item_id, &block)
+    itemEntry = 'https://readom-api.herokuapp.com/news/v0/%s.json' % item_id
 
-    AFMotion::JSON.get(listEntry) do |result|
+    AFMotion::JSON.get(itemEntry) do |result|
       if result.success?
-        loop_count = 0
-        item = result.object.sample
+        item = result.object
 
-        while item['url'].nil? and result.object.size >= loop_count+=1
-          item = result.object.sample
-        end
+        item_title = item['title']
+        item_url = item['url']
 
         block.call(item['id'], item['title'], item['url'])
       end
     end
   end
 
-  def self.fetch_items(list=:newstories, &block)
-    listEntry = 'https://Readom.github.io/HackerNewsJSON/%s.json' % list
+  def self.fetch_item_sample(list=:newstories, &block)
+    listEntry = 'https://readom-api.herokuapp.com/news/v0/%s.sample' % list
 
     AFMotion::JSON.get(listEntry) do |result|
       if result.success?
-        result.object.each do |item|
-          block.call(item['id'], item['title'], item['url']) unless item['url'].nil?
+        loop_count = 0
+        item_id = result.object.sample
+
+        fetch_item(item_id) do |item|
+          block.call(item['id'], item['title'], item['url'])
+        end
+      end
+    end
+  end
+
+  def self.fetch_items(list=:newstories, &block)
+    listEntry = 'https://readom-api.herokuapp.com/news/v0/%s.json' % list
+
+    AFMotion::JSON.get(listEntry) do |result|
+      if result.success?
+        result.object.each do |item_id|
+          fetch_item(item_id) do |item|
+            block.call(item['id'], item['title'], item['url']) unless item['url'].nil?
+          end
         end
       end
     end
