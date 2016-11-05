f410b342770caae775174c1e83bcb0009320fe4c
Liu Lantao <liulantao@gmail.com>
2016-11-05 17:10:32 +0800

    GiveMe5ViewController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,7 +3,8 @@ class AppDelegate
     tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
     tab_controller.viewControllers = [
       UINavigationController.alloc.initWithRootViewController(HomeViewController.new),
-      UINavigationController.alloc.initWithRootViewController(StoriesViewController.new)
+      UINavigationController.alloc.initWithRootViewController(StoriesViewController.new),
+      UINavigationController.alloc.initWithRootViewController(GiveMe5ViewController.new)
     ]
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
