cee6f3685be39be58a06bbe5572c84d3de3b39fa
Liu Lantao <liulantao@gmail.com>
2016-11-09 20:49:57 +0800

    style

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -11,6 +11,16 @@ class AppDelegate
     @window.rootViewController = tab_controller
     @window.makeKeyAndVisible
 
+    set_style()
+
     true
+  end
+
+private
+  def set_style
+    UITabBar.appearance.setTintColor UIColor.yellowColor
+    UITabBar.appearance.setBarTintColor UIColor.colorWithRed(0.45, green: 0.70, blue: 0.90, alpha: 1.0)
+    UITabBar.appearance.setBarStyle UIBarStyleBlack
+    UINavigationBar.appearance.setTitleTextAttributes(NSForegroundColorAttributeName => UIColor.colorWithRed(0.45, green: 0.70, blue: 0.90, alpha: 1.0))
   end
 end
