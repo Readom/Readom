d3fdce975e4e05c289c7e9c957e9363a1681a6c6
Liu Lantao <liulantao@gmail.com>
2016-11-27 15:18:46 +0800

    move button position to left

--- app/stylesheets/story_screen_stylesheet.rb
+++ app/stylesheets/story_screen_stylesheet.rb
@@ -35,17 +35,17 @@ class StoryScreenStylesheet < ApplicationStylesheet
     end
   end
 
+  def version_label(st)
+    st.frame = {w: 36, h: 10, fl: 0, t: 20}
+
+    st.font = UIFont.fontWithName('Arial', size: 9)
+  end
+
   def switch_topic_btn(st)
-    st.frame = {w: 28, h: 28, fr: 0, t: 35}
+    st.frame = {w: 28, h: 28, fl: 0, t: 35}
   end
 
   def refresh_btn(st)
-    st.frame = {w: 28, h: 28, fr: 0, t: 80}
-  end
-
-  def version_label(st)
-    st.frame = {w: 36, h: 10, fr: 0, t: 20}
-
-    st.font = UIFont.fontWithName('Arial', size: 9)
+    st.frame = {w: 28, h: 28, fl: 0, t: 80}
   end
 end
